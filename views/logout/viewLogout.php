965c83e11bd8b3cf499d9fb60579a90f3e65bdd7
rgeloen <rgeloen@hotmail.fr>
2018-04-10 21:54:26 +0200

    :sparkles: logout page is now beautiful, need to correct the displaying tho

--- views/logout/viewLogout.php
+++ views/logout/viewLogout.php
@@ -1,4 +1,6 @@
 <section class="page-logout">
+  <div class="message">
     <h1>Successful logout</h1>
     <p>Back to <a href="./" title="back to homepage">homepage</a> in <span class="timer">5 seconds</span></p>
+  </div>
 </section>
