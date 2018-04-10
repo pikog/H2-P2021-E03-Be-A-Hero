13319444aa469d4990b09ac1ef5be24e5e2bc29e
rgeloen <rgeloen@hotmail.fr>
2018-04-11 01:50:09 +0200

    :sparkles: Added Home w/profile of user

--- views/home/viewHome.php
+++ views/home/viewHome.php
@@ -1,8 +1,26 @@
-<section>
-    <h2><?= $user['heroName']; ?></h2>
-    <img src="./assets/images/heroes/<?= $user['hero']; ?>.png" alt="hero">
-    <h1><?= $user['level']; ?></h1>
-    <h1><?= $user['xp']; ?></h1>
-    <h1><?= $xpToNext; ?></h1>
-    <h1><?= $numberEventsSuccess; ?></h1>
+<section class="page-home">
+    <a href="./home"> <img class="logo" src="./assets/images/logo.png" alt="logo be a hero"> </a>
+
+    <div class="hero-infos">
+      <img class="hero" src="./assets/images/heroes/<?= $user['hero']; ?>.png" alt="hero">
+      <div class="infos">
+        <h2 class="name"><?= $user['heroName']; ?></h2>
+        <h2 class="level-title">LEVEL</h2>
+        <div class="level">
+          <h1 class="level-text"><?= $user['level']; ?></h1>
+        </div>
+      </div>
+    </div>
+
+
+    <!-- can i use = 97% on mobile -->
+    <div class="xp">
+      <meter class="meter" value="<?= $user['xp']; ?>" min="0" max="<?= $xpToNext; ?>"></meter>
+      <div class="xp-text">
+        <span class="current-xp"><?= $user['xp']; ?></span>
+        <span class="max-xp"><?= $xpToNext; ?></span>
+      </div>
+    </div>
+
+    <h1 class="events-success"><?= $numberEventsSuccess; ?> Victories</h1>
 </section>
