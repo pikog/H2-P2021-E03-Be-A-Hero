a502958902377442734ec6bc8b8fcbdfbe3aa586
rgeloen <rgeloen@hotmail.fr>
2018-04-11 14:12:02 +0200

    :lip-stick: responsive on home

--- views/geolocation/viewGeolocation.php
+++ views/geolocation/viewGeolocation.php
@@ -1,7 +1,8 @@
 <section class="page-geo">
     <div class="top-bar geo">
-      <a class="missions" href="#">PROFILE</a>
+      <a class="profile" href="#">PROFILE</a>
       <a class="logout" href="./logout">LOGOUT</a>
     </div>
+    <img class="logo" src="./assets/images/logo.png" alt="logo be a hero">
     <a class="button button-geolocation" href="#"><i class="fas fa-location-arrow"></i>Locate me</a>
 </section>
