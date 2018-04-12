0f9792cc32524f041334c33dfe4ffe89508dc154
Pikog <perso.pikog@gmail.com>
2018-04-12 15:56:01 +0200

    :art:Add logo on map

--- views/missions/viewMissions.php
+++ views/missions/viewMissions.php
@@ -3,10 +3,10 @@
 </script>
 <section class="page-mission not-geolocalised">
     <?php include './views/partials/_top-bar.php'; ?>
+    <div class="logo">
+        <a href="./home"><img src="./assets/images/logo.png" alt="logo be a hero"></a>
+    </div>
     <div class="content-not-geolocalised">
-        <div class="logo">
-            <a href="./home"><img src="./assets/images/logo.png" alt="logo be a hero"></a>
-        </div>
         <a class="button button-geolocation" href="#"><i class="fas fa-location-arrow"></i> Locate me</a>
     </div>
     <div class="popin">
